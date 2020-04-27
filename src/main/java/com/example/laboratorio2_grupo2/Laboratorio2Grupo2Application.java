761e9fde6cd35b85d2f47f41f2cd17f8065856f1
allisonvp <a.velardep@pucp.edu.pe>
2020-04-27 18:49:14 -0500

    inicio

---
+++ src/main/java/com/example/laboratorio2_grupo2/Laboratorio2Grupo2Application.java
@@ -0,0 +1,13 @@
+package com.example.laboratorio2_grupo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Laboratorio2Grupo2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Laboratorio2Grupo2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/laboratorio2_grupo2/Laboratorio2Grupo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.laboratorio2_grupo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Laboratorio2Grupo2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
