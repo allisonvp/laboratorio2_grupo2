3bd287227cd5891a26efc8cd2326d21411d87819
Royer <r.yangali@pucp.pe>
2020-04-27 19:41:49 -0500

    commit royer a branch principal (renombrando job a JobEntity)

--- src/main/java/com/example/laboratorio2_grupo2/Repository/JobRepository.java
+++ src/main/java/com/example/laboratorio2_grupo2/Repository/JobRepository.java
@@ -1,9 +1,9 @@
 package com.example.laboratorio2_grupo2.Repository;
 
-import com.example.laboratorio2_grupo2.Entity.Job;
+import com.example.laboratorio2_grupo2.Entity.JobEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface JobRepository extends JpaRepository<Job, String> {
+public interface JobRepository extends JpaRepository<JobEntity, String> {
 }
