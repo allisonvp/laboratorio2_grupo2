10abdb96f37e7ee5e034f6ae8332956d057c3d4a
allisonvp <a.velardep@pucp.edu.pe>
2020-04-27 20:59:47 -0500

    final

--- src/main/java/com/example/laboratorio2_grupo2/Controller/JobController.java
+++ src/main/java/com/example/laboratorio2_grupo2/Controller/JobController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -52,8 +53,14 @@ public class JobController {
 
     @GetMapping("/create")
     public String crearJob(){
-
+        return "job/crear";
     }
 
+
+    @PostMapping("/guardar")
+    public String guardarPais(JobEntity jobEntity) {
+        jobRepository.save(jobEntity);
+        return "redirect:/job/lista";
+    }
 
 }
